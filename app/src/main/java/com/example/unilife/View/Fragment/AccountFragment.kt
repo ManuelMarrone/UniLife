ab0dd3d11b2260063dd4b44022828a810de19f28
Luca-Dambro <75083183+Luca-Dambro@users.noreply.github.com>
2024-05-29 00:41:31 +0200

    modifica password e username

--- app/src/main/java/com/example/unilife/View/Fragment/AccountFragment.kt
+++ app/src/main/java/com/example/unilife/View/Fragment/AccountFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.example.unilife.View.Activity.AccessoActivity
+import com.example.unilife.View.Activity.ModificaActivity
 import com.example.unilife.ViewModel.AccountViewModel
 import com.example.unilife.databinding.FragmentAccountBinding
 import kotlinx.coroutines.launch
@@ -29,6 +30,8 @@ class AccountFragment : Fragment() {
         // Inflate the layout for this fragment
         viewBinding = FragmentAccountBinding.inflate(inflater, container, false)
         return viewBinding.root
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,6 +53,18 @@ class AccountFragment : Fragment() {
         viewBinding.esciAccountButton.setOnClickListener {
             viewModel.logOut()
         }
+
+        viewBinding.ChangeUsername.setOnClickListener {
+            // Chiamata alla funzione per navigare alla ModificaActivity
+            navigateToModificaActivity()
+        }
+    }
+
+    private fun navigateToModificaActivity() {
+        // Crea un intent per l'attività ModificaActivity
+        val intent = Intent(requireActivity(), ModificaActivity::class.java)
+        // Avvia l'attività ModificaActivity
+        startActivity(intent)
     }
 
     companion object {
